7a2163328e6c0f934c55d3fb886d77624fc66e96
awaisjameel <awais.jameel@ymail.com>
2026-03-25 21:37:46 +0500

    feat: upgrade laravel to v13

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 use App\Modules\Shared\Support\ModuleRegistry;
 use Illuminate\Broadcasting\BroadcastController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
-    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->withoutMiddleware([PreventRequestForgery::class])
     ->match(['get', 'post'], 'broadcasting/auth', [BroadcastController::class, 'authenticate']);
 
 Route::middleware('auth:sanctum')
-    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->withoutMiddleware([PreventRequestForgery::class])
     ->match(['get', 'post'], 'broadcasting/user-auth', [BroadcastController::class, 'authenticateUser']);
 
 $moduleRouteFiles = ModuleRegistry::apiRoutes(base_path());
